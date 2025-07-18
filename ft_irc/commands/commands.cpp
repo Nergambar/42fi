d7a881858d2efcdd3b064dd6582b6f8cca0cc562
Neri Gambarelli <negambar@c2r5p12.42firenze.it>
2025-07-18 15:13:33 +0200

    18/7/25

--- ft_irc/commands/commands.cpp
+++ ft_irc/commands/commands.cpp
@@ -2,12 +2,37 @@
 
 Command::Command()
 {
-    std::cout
+    std::cout << "Default constructor called" << std::endl;
 }
 
-Command::~Command(){}
+Command::~Command()
+{
+    std::cout << "Default destructor called" << std::endl;
+}
 
 Command::Command(const Command &src)
 {
+    std::cout << "Copy constructor called" << std::endl;
     *this = src;
+}
+
+Command     &Command::operator = (const Command &src)
+{
+    return (*this);
+}
+
+int Command::getCommand(std::string name)
+{
+    size_t which;
+
+    if (name == "KICK")
+        return(1);
+    else if (name == "INVITE")
+        return(2);
+    else if (name == "TOPIC")
+        return(3);
+    else if (name == "MODE")
+        return(4);
+    else
+        return(999);
 }
--- ft_irc/commands/commands.hpp
+++ ft_irc/commands/commands.hpp
@@ -8,12 +8,14 @@
 class Command
 {
     private:
+        std::string name;
         size_t type;
     public:
        Command();
        Command(const Command &src);
        Command &operator=(const Command &src);
        ~Command();
+       int     getCommand(std::string name);
 };
 
 #endif
